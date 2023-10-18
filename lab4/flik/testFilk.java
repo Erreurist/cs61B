c86a7c44ef08efb8e46d32b71dceddf89194a7c9
fuxinfeng <fuxinfengfxf@gmail.com>
2023-10-18 17:00:38 +0800

    lab4

---
+++ lab4/flik/testFilk.java
@@ -0,0 +1,10 @@
+package flik;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class testFilk {
+    @Test
+    public void test1() {
+        assertTrue(Flik.isSameNumber(128, 128));
+    }
+}
