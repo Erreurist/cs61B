4e55867882b3eea4ec6b48948f4443d99684fd27
fuxinfeng <fuxinfengfxf@gmail.com>
2023-10-25 21:58:22 +0800

    add error in main

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -12,47 +12,51 @@ public class Main {
     /** Usage: java gitlet.Main ARGS, where ARGS contains
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
-    public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
-            System.out.println("Please enter a command.");
-            System.exit(0);
-        }
-        String firstArg = args[0];
-        switch(firstArg) {
-            case "init":
-                Repository.init(args);
-                break;
-            case "add":
-                Repository.add(args);
-                break;
-            case "commit":
-                Repository.commit(args);
-                break;
-            case "rm":
-                break;
-            case "log":
-                Repository.log(args);
-                break;
-            case "global-log":
-                break;
-            case "find":
-                break;
-            case "status":
-                break;
-            case "checkout":
-                Repository.checkout(args);
-                break;
-            case "branch":
-                break;
-            case "rm-branch":
-                break;
-            case "reset":
-                break;
-            case "merge":
-                break;
-            default:
-                System.out.println("No command with that name exists.");
+    public static void main(String[] args) {
+        try {
+            if (args.length == 0) {
+                System.out.println("Please enter a command.");
                 System.exit(0);
+            }
+            String firstArg = args[0];
+            switch(firstArg) {
+                case "init":
+                    Repository.init(args);
+                    break;
+                case "add":
+                    Repository.add(args);
+                    break;
+                case "commit":
+                    Repository.commit(args);
+                    break;
+                case "rm":
+                    break;
+                case "log":
+                    Repository.log(args);
+                    break;
+                case "global-log":
+                    break;
+                case "find":
+                    break;
+                case "status":
+                    break;
+                case "checkout":
+                    Repository.checkout(args);
+                    break;
+                case "branch":
+                    break;
+                case "rm-branch":
+                    break;
+                case "reset":
+                    break;
+                case "merge":
+                    break;
+                default:
+                    System.out.println("No command with that name exists.");
+                    System.exit(0);
+            }
+        } catch (IOException e) {
+            throw error("An I/O error occurred: " + e.getMessage());
         }
     }
 }
