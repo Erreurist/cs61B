db111ade1c4baad4eebd91f8c786b84ca2b4beb7
fuxinfeng <fuxinfengfxf@gmail.com>
2023-11-05 11:22:05 +0800

    finish lab7

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -1,38 +1,82 @@
 package bstmap;
-
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Set;
 
-public class BSTMap<K extends Comparator<K>, V> implements Map61B<K, V> {
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+    private int size;
+    private BSTNode root;
 
     private class BSTNode {
         private K key;
         private V value;
+        private BSTNode left;
+        private BSTNode right;
+
+        BSTNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public BSTMap() {
+        this.size = 0;
+        this.root = null;
     }
     @Override
     public void clear() {
-
+        size = 0;
+        root = null;
     }
 
     @Override
     public boolean containsKey(K key) {
-        return false;
+        return get(key) != null;
     }
+
 
     @Override
     public V get(K key) {
-        return null;
+        return get(root, key);
+    }
+
+    private V get(BSTNode node, K key) {
+        if (node == null) {
+            return null;
+        }
+        if (node.key.equals(key)) {
+            return node.value;
+        }
+        if (node.key.compareTo(key) < 0) {
+            return get(node.left, key);
+        }
+        else {
+            return get(node.right, key);
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public void put(K key, V value) {
+        size++;
+        put(root, key, value);
+    }
 
+    private BSTNode put(BSTNode node, K key, V value) {
+        if (node == null) {
+            return new BSTNode(key, value);
+        }
+        if (key.compareTo(node.key) < 0) {
+            node.left = put(node.left, key, value);
+        }
+        if (key.compareTo(node.key) > 0) {
+            node.right = put(node.right, key, value);
+        }
+        node.value = value;
+        return node;
     }
 
     @Override
